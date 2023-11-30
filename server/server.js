45f6c319b1a19c96c0b700aad9a8b9c180c12f61
ryanbarillos <ryan.barillos@proton.me>
2023-11-30 11:55:53 -0700

    Changes

--- server/server.js
+++ server/server.js
@@ -1,28 +1,26 @@
+import account from "./credentials.js";
+
 const express = require("express"),
   app = express(),
   port = 4000,
+  // account = require("../env"),
   mongoose = require("mongoose");
 
 app.get("/", (request, response) => {
   response.send("Hi");
 });
 
-app.get("/blog", (request, response) => {
-  response.send("Net Ninja");
-});
-
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+  console.log(account.name);
 });
 
-mongoose.connnect;
-// const express = require("express"),
-//   app = express();
-
-// app.get("/api", (req, res) => {
-//   res.json({ usernames: ["Ali", "Mia", "Jay", "Max", "John"] });
-// });
-
-// app.listen(3001, () => {
-//   console.log("Port 3001");
-// });
+// mongoose
+//   .connect(
+//     `mongodb+srv://${account.user}:${account.pass}@webproject.xe9schq.mongodb.net/`
+//   )
+//   .then(() => {
+//     console.log("MonkeyDB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
