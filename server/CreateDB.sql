b8e917ba8c2c9f54ebbb1ec41e30c2fbb58a6358
ryanbarillos <ryan.barillos@proton.me>
2023-11-30 16:55:58 -0700

    Move CreateDB.sql to new directory

--- server/CreateDB.sql
+++ server/CreateDB.sql
@@ -42,8 +42,8 @@ CREATE TABLE Students
 
 CREATE TABLE CoursesEnrolled
 (
-    studentID INT NOT NULL FOREIGN KEY REFERENCES studentID(Students),
-    courseID INT NOT NULL FOREIGN KEY REFERENCES courseID(Courses),
+    studentID INT NOT NULL FOREIGN KEY REFERENCES Students(studentID),
+    courseID INT NOT NULL FOREIGN KEY REFERENCES Courses(courseID)
 );
 
 CREATE TABLE Administrators
@@ -62,20 +62,36 @@ CREATE TABLE Administrators
 INSERT INTO Courses
     (courseName, courseCode,courseTerm)
 VALUES
-    ("Project Management 1", "PRO111", 1),
-    ("C++ Programming Fundamentals", "CPP111", 1),
-    ("Computer Maintenance 1", "COMP111", 1),
-    ("Information Security 1", "IS111", 1),
-    ("Networking Basics", "NET222", 2),
-    ("Web Technology", "WEB222", 2),
-    ("Project Management 2", "PRO222", 2),
-    ("Advanced Project Management 1", "PRO333", 3),
-    ("Advanced C++ Programming Fundamentals", "CPP333", 3),
-    ("Advanced Computer Maintenance", "COMP333", 3),
-    ("Advanced Information Security 1", "IS333", 3),
-    ("Advanced Networking", "NET444", 4),
-    ("Advanced Computer Maintenance", "COMP444", 4),
-    ("Advanced Information Security 1", "WEB444", 4);
+    ('Project Management 1', 'PRO111', 1),
+    ('C++ Programming Fundamentals', 'CPP111', 1),
+    ('Computer Maintenance 1', 'COMP111', 1),
+    ('Information Security 1', 'IS111', 1),
+    ('Networking Basics', 'NET222', 2),
+    ('Web Technology', 'WEB222', 2),
+    ('Project Management 2', 'PRO222', 2),
+    ('Advanced Project Management 1', 'PRO333', 3),
+    ('Advanced C++ Programming Fundamentals', 'CPP333', 3),
+    ('Advanced Computer Maintenance 1', 'COMP333', 3),
+    ('Advanced Information Security 1', 'IS333', 3),
+    ('Advanced Networking', 'NET444', 4),
+    ('Advanced Computer Maintenance 2', 'COMP444', 4),
+    ('Advanced Information Security 2', 'IS444', 4);
+
+INSERT INTO Students
+    (studentTerm, studentEmail, studentPasswd, studentNameFirst, studentNameLast)
+VALUES
+    (1, 'back@bvc.ca', 'backslide', 'Back', 'Slide'),
+    (3, 'hyung@bvc.ca', 'hugology', 'Hyung', 'Su');
+
+INSERT INTO Administrators
+    (adminEmail, adminPasswd, adminNameFirst, adminNameLast)
+VALUES
+    ('jdm@bvc.ca', 're@|!+7_sets', 'Jon', 'Doe'),
+    ('arkfx@bvc.ca', 'arkenfoxjs', 'Arken', 'Fox');
+
+SELECT *
+FROM Courses
+WHERE courseTerm = 1;
 -- Data Population END
 USE master
 GO
