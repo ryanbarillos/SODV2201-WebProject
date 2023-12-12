dbc528e071203bf143730eee0738595cea6717c0
ryanbarillos <ryan.barillos@proton.me>
2023-12-11 18:37:28 -0700

    Couse List now only works with authenticated user

--- client/src/pages/student/components/CourseList.js
+++ client/src/pages/student/components/CourseList.js
@@ -1,17 +1,27 @@
+import useAuthContext from "../../../hooks/useAuthContext";
+
+import { useState } from "react";
 const CourseList = ({ courses, term }) => {
-  let list = courses.filter((c) => c.courseTerm === term);
+  const user = useAuthContext().user,
+    [list, setList] = useState(courses.filter((c) => c.CourseTerm === term)),
+    lsUpdate = (index) => {
+      alert("You've enrolled in this course!");
+      setList(list.filter((c) => c.CourseID !== index));
+    };
   return (
     <div>
+      <h1>You are in Term {term}</h1>
+      <h2>Select Your Course(s)</h2>
       {list.map((c) => (
         // Render each course into individual sections
         <div className="courseList">
-          <h2 style={{ textAlign: "left", color: "red" }}>
-            {c.courseName}
+          <h2 style={{ textAlign: "left", color: "hsla(335, 100%, 40%, 1)" }}>
+            {c.CourseName}
             <span style={{ float: "right", color: "black" }}>
-              {c.courseCode}
+              {c.CourseCode}
             </span>
           </h2>
-          <button>Add Course</button>
+          <button onClick={() => lsUpdate(c.CourseID)}>Add Course</button>
         </div>
       ))}
     </div>
--- client/src/pages/student/pages/AddCouses.js
+++ client/src/pages/student/pages/AddCouses.js
@@ -1,28 +1,32 @@
 import React, { useEffect, useState } from "react";
 import CourseList from "../components/CourseList";
+import useAuthContext from "../../../hooks/useAuthContext";
 
 function AddCourses() {
   const [termNow, termNowSet] = useState(1),
     [termNext, termNextSet] = useState(termNow === 4 ? 0 : termNow + 1),
-    [courses, setCourses] = useState(null);
+    [courses, setCourses] = useState(null),
+    user = useAuthContext().user;
 
   // Get courses
   useEffect(() => {
-    fetch("/api/course/all")
-      .then((response) => {
-        return response.json();
+    if (user) {
+      fetch("/api/course/all", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
       })
-      .then((data) => {
-        setCourses(data);
-      });
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setCourses(data);
+        });
+    }
   }, []);
 
   return (
-    <div>
-      <h1>You are in Term {termNow}</h1>
-      <h2>Select Your Courses</h2>
-      {courses && <CourseList courses={courses} term={termNow} />}
-    </div>
+    <div>{courses && <CourseList courses={courses} term={termNow} />}</div>
   );
 }
 
